67a4e0a6e2cf875941032854c46932ee7e0e7569
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2023-02-01 19:44:31 +0200

    sequelize init

---
+++ controllers/hero.controller.js
@@ -0,0 +1,19 @@
+module.exports.createHero = async (req, res, next) => {
+
+};
+
+module.exports.updateHero = async (req, res, next) => {
+    
+};
+
+module.exports.deleteHero = async (req, res, next) => {
+    
+};
+
+module.exports.getAllHeroes = async (req, res, next) => {
+    
+};
+
+module.exports.getHeroWithSuperPower = async (req, res, next) => {
+    
+};
